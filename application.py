6af3e75ef96112a5b5fb3181256736118599c96a
busgandy <39296111+busgandy@users.noreply.github.com>
2020-08-20 01:25:37 +0100

    Update application.py

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import pandas as pd
 import random
 from flask import Flask, render_template, request
 
-raw = (pd.read_excel("https://github.com/busgandy/gideonbusayo/blob/master/ranking.xlsx?raw=true"))
+raw = (pd.read_excel("ranking.xlsx"))
 
 
 app = Flask(__name__)
@@ -17,7 +17,7 @@ def hello():
     hello = raw.loc[raw['Student ID'] == name]
     if not name:
         render_template("failure.html)
-    for items in hello:
-        if name != items:
-            render_template("failure.html)
+    #for items in hello:
+        #if name != items:
+            #render_template("failure.html)
     return render_template("hello.html", hello = name)
